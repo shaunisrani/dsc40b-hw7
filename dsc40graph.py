d28c909065067bf419aba1e1eeccbe24a06a213c
shaunisrani <sisrani@ucsd.edu>
2025-11-13 19:33:22 -0800

    yes

--- dsc40graph.py
+++ dsc40graph.py
@@ -2,10 +2,12 @@ import collections
 
 class DirectedGraph:
     """
-    A simple implementation of a directed graph using an adjacency list.
+    A simple implementation of a directed graph.
     """
     def __init__(self):
+        # Stores {node: [neighbor1, neighbor2]}
         self.adj = collections.defaultdict(list)
+        # Stores all nodes, including those with no edges
         self._nodes = set()
 
     def add_edge(self, u, v):
@@ -13,11 +15,18 @@ class DirectedGraph:
         self.adj[u].append(v)
         self._nodes.add(u)
         self._nodes.add(v)
+    
+    def add_node(self, u):
+        """Adds a node to the graph."""
+        if u not in self._nodes:
+            self._nodes.add(u)
+            # Ensures the node exists in the adjacency dict
+            _ = self.adj[u]
 
     def neighbors(self, u):
         """Returns the neighbors of node u in ascending order."""
-        # The homework specifies neighbors should be produced in ascending order
-        return sorted(self.adj[u])
+        # Use .get() for safety in case a node exists but has no outgoing edges
+        return sorted(self.adj.get(u, []))
 
     @property
     def nodes(self):
@@ -27,7 +36,7 @@ class DirectedGraph:
 class UndirectedGraph:
     """
     A simple implementation of an undirected graph.
-    (You will need this for Problem 2: Clustering)
+    (Needed for Programming Problem 2: Clustering)
     """
     def __init__(self):
         self.adj = collections.defaultdict(list)
@@ -40,9 +49,16 @@ class UndirectedGraph:
         self._nodes.add(u)
         self._nodes.add(v)
 
+    def add_node(self, u):
+        """Adds a node to the graph."""
+        if u not in self._nodes:
+            self._nodes.add(u)
+            # Ensures the node exists in the adjacency dict
+            _ = self.adj[u]
+
     def neighbors(self, u):
         """Returns the neighbors of node u in ascending order."""
-        return sorted(self.adj[u])
+        return sorted(self.adj.get(u, []))
 
     @property
     def nodes(self):
